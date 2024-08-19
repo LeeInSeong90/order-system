d327d902ff9f75dfd7c6a4f592b835ed4fa18220
LeeInSeong90 <insg9043@gamil.com>
2024-08-19 18:50:57 +0900

    init

--- src/main/java/com/example/ordersystem/domain/StoreProduct.java
+++ src/main/java/com/example/ordersystem/domain/StoreProduct.java
@@ -1,22 +1,31 @@
 package com.example.ordersystem.domain;
 
 
+import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
+@Getter
 @Table(name = "store_Products")
 public class StoreProduct {
     @Id
-    private String storeProductId;
+    private int storeProductId;
 
     @Column
-    private String storeId;
+    private int storeId;
 
     @Column
-    private String productId;
+    private int productId;
 
     @Column
-    private String stockQuantity;
+    private int stockQuantity;
 
+
+    public void adjustStockQuantity(int buyQuantity){
+        if(buyQuantity > stockQuantity){
+            throw new RuntimeException("재고보다 많을 수 없습니다.");
+        }
+        this.stockQuantity = this.stockQuantity - buyQuantity;
+    }
 }
--- src/main/resources/data/schema.sql
+++ src/main/resources/data/schema.sql
@@ -2,8 +2,6 @@ SET foreign_key_checks = 0;
 
 DROP TABLE IF EXISTS products;
 
-
-
 CREATE TABLE products
 (
     product_id  INT             NOT NULL AUTO_INCREMENT,
@@ -18,7 +16,7 @@ DROP TABLE IF EXISTS orders;
 CREATE TABLE orders
 (
     order_id    INT     NOT NULL AUTO_INCREMENT,
-    customer    INT     NOT NULL,
+    customer_id    INT     NOT NULL,
     ordered_at  DATETIME NOT NULL,
     PRIMARY KEY (order_id)
 );
@@ -44,6 +42,28 @@ CREATE TABLE customers
     phone_number VARCHAR(100)    NULL,
     address      VARCHAR(100)    NOT NULL,
     PRIMARY KEY (customer_id)
+);
+
+DROP TABLE IF EXISTS stores;
+CREATE TABLE stores
+(
+    store_id        INT          NOT NULL AUTO_INCREMENT,
+    name            VARCHAR(100) NOT NULL,
+    address         VARCHAR(100) NOT NULL,
+    phone_number    VARCHAR(100) NOT NULL,
+    open_at         VARCHAR(100) NOT NULL,
+    close_at        VARCHAR(100) NOT NULL,
+    PRIMARY KEY (store_id)
+);
+
+DROP TABLE IF EXISTS store_products;
+CREATE TABLE store_products
+(
+    store_product_id    INT NOT NULL AUTO_INCREMENT,
+    store_id            INT NOT NULL,
+    product_id          INT NOT NULL,
+    stock_quantity      INT NOT NULL,
+    PRIMARY KEY (store_product_id)
 );
 
 
